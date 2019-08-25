78e643b513d60c8de4513af13bc85de037807694
ReyesAntonioLuisF <lfrareyes19@gmail.com>
2019-08-25 01:32:15 -0500

    prueba boton unity

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Your web app's Firebase configuration
-/*var firebaseConfig = {
+var firebaseConfig = {
     apiKey: "AIzaSyCs7PUMT3SrA5fHc8h6KyS0Wv_cqrHVlmc",
     authDomain: "arelektra-cb680.firebaseapp.com",
     databaseURL: "https://arelektra-cb680.firebaseio.com",
@@ -7,40 +7,57 @@
     storageBucket: "",
     messagingSenderId: "335620122141",
     appId: "1:335620122141:web:593b4cbd15e04414"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-*/
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+function insertarTask() {
+    let id = 1
+    let nombre: "Antena Plana Para Interiores Master TVANTFLATHD"
+    let height: 27
+    let length: 50
+    let weight: 430
+    let foto: "https://elektraqapre.vteximg.com.br/arquivos/ids/157064-292-292/177602.jpg?v=636568020153600000"
+    let price: "$199.00"
+    let descripcion: "Antena Plana Para Interiores Master TVANTFLATHD"
+    let disponible: true;
+
+    let arrayData = {nombre: nombre, alto: height, largo: length, ancho: weight, foto: foto, precio: price, descripcion: descripcion, disponibilidad: disponible};
+    var task = firebase.database().ref("task/"+id);
+    task.set(arrayData)
+    
+}
+
 var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
 function isIOS() {
-  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-      return true;
-  } else {
-    return false;
-  }
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function isAndroid() {
-  return (/android/i.test(userAgent));
+    return (/android/i.test(userAgent));
 }
 
 function appendIframeWithURL(url) {
-  var iframe = document.createElement("IFRAME");
-  iframe.setAttribute("src", url);
-  document.documentElement.appendChild(iframe);
-  iframe.parentNode.removeChild(iframe);
-  iframe = null;
+    var iframe = document.createElement("IFRAME");
+    iframe.setAttribute("src", url);
+    document.documentElement.appendChild(iframe);
+    iframe.parentNode.removeChild(iframe);
+    iframe = null;
 }
 
 function sendMessageToUnity(message) {
-  if (isIOS()) {
-    appendIframeWithURL('inappbrowserbridge://' + message);
-  } else if (isAndroid()){
-    UnityInAppBrowser.sendMessageFromJS(message);
-  }
+    if (isIOS()) {
+        appendIframeWithURL('inappbrowserbridge://' + message);
+    } else if (isAndroid()) {
+        UnityInAppBrowser.sendMessageFromJS(message);
+    }
 }
 
 function sendPing() {
-  sendMessageToUnity("ping");
+    sendMessageToUnity("ping");
 }
