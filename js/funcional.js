dfdb99e09d394b68796a9e2917f9e97815f48f52
ReyesAntonioLuisF <lfrareyes19@gmail.com>
2019-08-25 04:00:39 -0500

    prueba boton unity

---
+++ js/funcional.js
@@ -0,0 +1,47 @@
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+function isIOS() {
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      return true;
+  } else {
+    return false;
+  }
+}
+
+function isAndroid() {
+  return (/android/i.test(userAgent));
+}
+
+function appendIframeWithURL(url) {
+  var iframe = document.createElement("IFRAME");
+  iframe.setAttribute("src", url);
+  document.documentElement.appendChild(iframe);
+  iframe.parentNode.removeChild(iframe);
+  iframe = null;
+}
+
+function sendMessageToUnity(message) {
+  if (isIOS()) {
+    appendIframeWithURL('inappbrowserbridge://' + message);
+  } else if (isAndroid()){
+    UnityInAppBrowser.sendMessageFromJS(message);
+  }
+}
+
+function sendPing(cadena) {
+  sendMessageToUnity(cadena);
+}
